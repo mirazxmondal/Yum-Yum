dd4a5ae0a304bd9cf95d2be7fb5b7f6682cc8f98
miraz-mondal <mirazmondal.official@gmail.com>
2023-03-31 00:51:23 +0530

    Accordion added

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,1 +1,28 @@
 console.log("Landing-Page")
+
+// FAQs Section
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var parent = this.parentElement;
+    var panel = this.nextElementSibling;
+    var mainPanel = document.querySelector(".main-panel");
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+      parent.style.maxHeight =
+        parseInt(parent.style.maxHeight) + panel.scrollHeight + "px";
+    }
+  });
+}
+
+const hamburger = document.querySelector(".hamburger-menu");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", function () {
+  navLinks.classList.toggle("show");
+});
