10da31440ec2d4937be2925d637a84dfdf7424ff
miraz-mondal <mirazmondal.official@gmail.com>
2023-04-06 03:23:21 +0530

    Login/register page UI done

--- Assets/JS/loginregister.js
+++ Assets/JS/loginregister.js
@@ -1,1 +1,17 @@
 console.log("Login/Register-Page")
+
+// Form Validation
+
+
+// Form interactiveness
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
