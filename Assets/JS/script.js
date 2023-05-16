7c7c20197beb00be4c8abed6e028f239ecaf0790
miraz-mondal <mirazmondal.official@gmail.com>
2023-05-17 00:41:48 +0530

    4 new restaurant card added in the index page

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -29,7 +29,6 @@ function myFunction(x) {
   x.classList.toggle("change");
 }
 
-
 // Find Nearby Cuisines Section
 const restaurantsData = [
   {
@@ -55,7 +54,7 @@ const restaurantsData = [
       img: "Assets/Image/Restaurant-banner/pizza.png",
       "restaurant name": "WOW PIZZA",
       "restaurant info":
-        "Yum Yum brings together the finest pizzerias in town, ensuring that you experience the authentic flavors and craftsmanship that make pizza an all-time favoriteWhether you're a fan of thin and crispy, deep-dish, or gluten-free crusts, Yum Yum has an extensive selection to cater to your preferences.",
+        "Yum Yum brings together the finest pizzerias in town, ensuring that you experience the authentic flavors and craftsmanship that make pizza an all-time favoriteWhether you're a fan of thin and crispy, deep-dish, or gluten-free crusts.",
       button: "Restaurant Page",
     },
   },
@@ -65,6 +64,42 @@ const restaurantsData = [
       "restaurant name": "WOW PASTA",
       "restaurant info":
         "Whether you prefer creamy Alfredo, tangy marinara, or hearty Bolognese, Yum Yum offers an extensive selection of pasta dishes to suit every palate. Customize your pasta with a range of toppings, including succulent meats, vibrant vegetables, and aromatic herbs.",
+      button: "Restaurant Page",
+    },
+  },
+  {
+    restaurant: {
+      img: "Assets/Image/Restaurant-banner/fish.png",
+      "restaurant name": "WOW BENGALI",
+      "restaurant info":
+        "Bengali cuisine is renowned for its rich flavors, vibrant colors, and diverse range of dishes. Rooted in the fertile land of Bengal, which spans across Bangladesh and the Indian state of West Bengal, Bengali food showcases a harmonious blend of spices, fresh ingredients, and distinct cooking techniques.",
+      button: "Restaurant Page",
+    },
+  },
+  {
+    restaurant: {
+      img: "Assets/Image/Restaurant-banner/north-indian..png",
+      "restaurant name": "WOW NORTH INDIAN",
+      "restaurant info":
+        "North Indian cuisine is a tapestry of diverse flavors, aromatic spices, and rich culinary traditions. With its roots in the fertile plains of the Indo-Gangetic region, North Indian food offers a delightful array of dishes that captivate the senses and leave a lasting impression. Tandoori chicken, marinated in yogurt and spices, and succulent kebabs are iconic examples of the culinary.",
+      button: "Restaurant Page",
+    },
+  },
+  {
+    restaurant: {
+      img: "Assets/Image/Restaurant-banner/south-indian.png",
+      "restaurant name": "WOW SOUTH INDIAN",
+      "restaurant info":
+        "South Indian cuisine is a delectable journey that tantalizes taste buds with its vibrant flavors and diverse range of dishes. Known for its generous use of spices and aromatic ingredients, South Indian food is a delightful combination of traditional recipes and regional specialties. One of the staples of South Indian cuisine is dosa, a thin.",
+      button: "Restaurant Page",
+    },
+  },
+  {
+    restaurant: {
+      img: "Assets/Image/Restaurant-banner/chaat.png",
+      "restaurant name": "WOW CHAAT",
+      "restaurant info":
+        "Whether you prefer creamy Alfredo, tangy marinara, or hearty Bolognese, Yum Yum offers an extensive selection of pasta dishes to suit every palate. Customize your pasta with a range of toppings, including succulent meats, vibrant vegetables, and aromatic herbs.The essence of chaat lies in its vibrant assortment of ingredients.",
       button: "Restaurant Page",
     },
   },
@@ -114,3 +149,19 @@ restaurantsData.forEach((restaurant) => {
   // Append the card element to the container
   container.appendChild(card);
 });
+
+
+/**
+ * This function gets the value of a search input, stores it in localStorage, and redirects to a search
+ * page.
+ */
+function search() {
+  // Get the search input value
+  var searchValue = document.getElementById("searchInput").value.toLowerCase();
+
+  // Store the search value in localStorage to access it on the search page
+  localStorage.setItem("searchValue", searchValue);
+
+  // Redirect to the search page
+  window.location.href = "Assets/HTML/restaurant.html";
+}
